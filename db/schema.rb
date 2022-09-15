e74c03f6728f9c8ccbfc69322fd89a29f959b109
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-15 11:15:13 +0530

    Create Comments with proper contraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_051510) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_053651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "posts_id", null: false
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_051510) do
     t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
