c95af06a1ac678232165fed13712bdffefd0ed17
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-15 22:07:59 +0530

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_save :update_post_comments_count
+  after_save :update_post_comments_count
 
-    def update_post_comments_count
-        post.increment!(:comments_conter)
-    end
+  def update_post_comments_count
+    post.increment!(:comments_conter)
+  end
 end
