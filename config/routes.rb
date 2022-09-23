426a9459543aa37443edcf3c28759d380109654a
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-23 23:53:42 +0530

    Add comment markup and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      resources :comments, only: [:create, :new, :create, :new]
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :comments, only: [:create, :new]
       resources :likes, only: [:create]
     end
   end
