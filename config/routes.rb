f13c7f60f3cec7a2e1d97333535eaed4475a2905
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-07 16:23:53 +0530

    Update issues for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root "users#index"
+
+  devise_scope :user do get '/users/sign_out' => 'devise/sessions#destroy' end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
       resources :comments, only: [:create, :new]
