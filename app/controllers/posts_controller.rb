7eb95e938230a71c6ee4d7dc8658b272cf10791d
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-07 13:14:41 +0530

    Setup current_user using devise, and update flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,9 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html do
         if @new_post.save
-          redirect_to "/users/#{@new_post.author.id}/posts/", flash: { alert: 'Your post is saved' }
+          redirect_to "/users/#{@new_post.author.id}/posts/", flash: { alert: 'Post saved' }
         else
-          redirect_to "/users/#{@new_post.author.id}/posts/new", flash: { alert: 'Could not save post' }
+          redirect_to "/users/#{@new_post.author.id}/posts/new", flash: { alert: 'Can not save post' }
         end
       end
     end
