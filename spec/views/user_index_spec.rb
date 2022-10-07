51790036910d91ec5f911c7bc668f203d7879150
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-07 12:51:27 +0530

    Update tests

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 RSpec.describe 'users#index', type: :feature do
   describe 'user index page' do
     before(:each) do
-      @user1 = User.create(name: 'Rito', photo: 'Tom.png', bio: 'bio', posts_counter: 0)
-      @user2 = User.create(name: 'Amy', bio: 'bio', photo: 'Tom.png')
-      @user3 = User.create(name: 'Jerry', bio: 'bio', photo: 'Tom.png')
+      @user1 = User.create(name: 'Rito', email: 'rito@gmail.com',
+        password: 'password', photo: 'Tom.png', bio: 'bio', posts_counter: 0)
+      @user2 = User.create(name: 'Amy', email: 'amy@gmail.com',
+                          password: 'password' bio: 'bio', photo: 'Tom.png')
+      @user3 = User.create(name: 'Jerry', email: 'jerry@gmail.com',
+        password: 'password' bio: 'bio', photo: 'Tom.png')
 
       visit root_path
+      fill_in 'Email', with: 'rito@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
     end
 
     it "Shows the user's photo" do
--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 RSpec.describe 'user#Show', type: :feature do
   describe 'User Model' do
     before(:each) do
-      @user1 = User.create(name: 'Rito', photo: 'Tom.png', bio: 'bio', posts_counter: 0)
-      @user2 = User.create(name: 'Tom', bio: 'bio', photo: 'Tom.png')
-      @user3 = User.create(name: 'Jerry', bio: 'bio', photo: 'Tom.png')
+      @user1 = User.create(name: 'Rito', photo: 'Tom.png', bio: 'bio', posts_counter: 0, email: 'rito@gmail.com',
+                           password: 'password')
+      @user2 = User.create(name: 'Tom', email: 'tom@gmail.com',
+        password: 'password' bio: 'bio', photo: 'Tom.png')
+      @user3 = User.create(name: 'Jerry', email: 'jerry@gmail.com',
+        password: 'password', bio: 'bio', photo: 'Tom.png')
 
       visit root_path
+      fill_in 'Email', with: 'rito@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
 
       @post1 = Post.create(title: 'First Post', text: 'This is my first post', comments_conter: 0, likes_counter: 0,
                            author: @user1)
