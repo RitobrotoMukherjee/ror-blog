6dd974779c13fec72ec507ccc83ac683827757b1
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-07 16:05:25 +0530

    Fix error in sign up

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_060609) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter"
+    t.string "photo", default: "https://cdn1.iconfinder.com/data/icons/random-115/24/person-512.png"
+    t.text "bio", default: ""
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -57,6 +57,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_060609) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
